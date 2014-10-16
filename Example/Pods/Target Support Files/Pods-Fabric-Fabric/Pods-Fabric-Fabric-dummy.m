4915ea753a175b5984aef9c11f51d1e3fb04345d
Nico Hämäläinen <nico@sizeof.io>
2014-10-16 21:35:24 +0300

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-Fabric-Fabric/Pods-Fabric-Fabric-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fabric_Fabric : NSObject
+@end
+@implementation PodsDummy_Pods_Fabric_Fabric
+@end
---
+++ Example/Pods/Target Support Files/Pods-Fabric/Pods-Fabric-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Fabric
+#define COCOAPODS_POD_AVAILABLE_Fabric
+#define COCOAPODS_VERSION_MAJOR_Fabric 0
+#define COCOAPODS_VERSION_MINOR_Fabric 1
+#define COCOAPODS_VERSION_PATCH_Fabric 0
+
